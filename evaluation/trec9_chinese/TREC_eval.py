58d3bbe47c85404a7075a3bd0369ebc07800481d
Sachith <kothursachith@gmail.com>
2017-11-02 19:12:32 -0400

    Added config file for trec evaluation script. Added __init__.py in trec indexing folder

--- evaluation/trec9_chinese/TREC_eval.py
+++ evaluation/trec9_chinese/TREC_eval.py
@@ -7,11 +7,8 @@ import subprocess
 import collections
 import matplotlib.pyplot as plt
 from bs4 import BeautifulSoup
-
-#include path to index and search code
-sys.path.append('/home/sachith/CLIR/DUH/elastic4clir/index_collection/trec9_chinese')
-from TREC_search import search
-
+import importlib
+from configparser import SafeConfigParser
 
 #Given a query file, it maps the query number to the query
 def get_queries(query_file):
@@ -54,7 +51,7 @@ def get_queries(query_file):
 #Runs evaluation given queries, gold output and our output.
 #Returns the path to result file
 
-def prec_recall_graph(FIN_OUT):
+def prec_recall_graph(output_path, FIN_OUT):
     with open(FIN_OUT , 'r') as f:
         MAP = 0
         prec = [0]*11
@@ -78,11 +75,12 @@ def prec_recall_graph(FIN_OUT):
         plt.annotate('Mean Average Precision\n(MAP) = '+ str(MAP), xy=(0.4, MAP), xytext=(0.6, max(prec)/3),\
             arrowprops=dict(facecolor='black', shrink=0.05),\
             )
-        plt.savefig('P_r_graph_TREC.png')
+        plt.savefig(os.path.join(output_path,'P_r_graph_TREC.png'))
 
 
-def eval(query_file, ref_out_file, SEARCH_OUT, TREC_PATH):
-    f_out = open(SEARCH_OUT,'w');
+def eval(query_file, ref_out_file, output_path, TREC_PATH, search):
+    SEARCH_OUT = os.path.join(output_path, "search_output_trec.txt")
+    f_out = open(SEARCH_OUT,'w')
     queries = get_queries(query_file)
     if queries is None or len(queries) == 0:
         print ("\nInvalid or Bad Query File. Exiting Evaluation module\n")
@@ -107,28 +105,47 @@ def eval(query_file, ref_out_file, SEARCH_OUT, TREC_PATH):
         sys.exit
     
     #File to store final output
-    FIN_OUT = "results_TREC.txt"
+    FIN_OUT = os.path.join(output_path, "results_trec.txt")
     fin_out = open(FIN_OUT, 'w')
     print (subprocess.list2cmdline([TREC_EXEC, ref_out_file, SEARCH_OUT]))
     subprocess.call([TREC_EXEC, ref_out_file, SEARCH_OUT], stdout = fin_out)
     fin_out.close()
-    prec_recall_graph(FIN_OUT)
+    prec_recall_graph(output_path, FIN_OUT)
     return FIN_OUT
     
 
 if __name__ == '__main__':
-    USAGE = '\nUSAGE : python TREC_eval.py <query_file> <ref_output> \n'
+    USAGE = '\n USAGE : python TREC_eval.py <config_file> \n'
     
-    #Path to TREC_Eval C-code
-    TREC_PATH = '/home/sachith/CLIR/DUH/elastic4clir/evaluation/trec_eval.9.0'
-    
-    #File to store search output
-    SEARCH_OUT = "search_output_TREC.txt"
-    
-
-        
-    if len(sys.argv) != 3:
+    if len(sys.argv) != 2:
         print (USAGE)
         sys.exit()
     
-    eval(sys.argv[1], sys.argv[2], SEARCH_OUT, TREC_PATH)
+    parser = SafeConfigParser({'output_path' : os.getcwd()})
+    parser.read(sys.argv[1])
+    
+    if not (parser.has_option('Evaluation', 'search_script') or 
+            parser.has_option('Evaluation', 'trec_eval_path') or
+            parser.has_option('Evaluation', 'query_file') or
+            parser.has_option('Evaluation', 'reference_file')):
+        print ("Invalid/Incomplete Evaluation parameters in config file")
+        sys.exit()
+
+    search_script = parser.get('Evaluation', 'search_script')
+    TREC_PATH = parser.get('Evaluation', 'trec_eval_path')
+    query_file = parser.get('Evaluation', 'query_file')
+    reference_file = parser.get('Evaluation', 'reference_file')
+    output_path = parser.get('Evaluation', 'output_path')
+    
+    #Import search module
+    search_dir, search_file = os.path.split(search_script)
+    if search_dir != '':
+        sys.path.append(search_dir)
+    if search_file == '':
+        print ("Invalid search_script provided in Evaluation config")
+        sys.exit()
+    else:
+        mod = importlib.import_module(search_file.replace('.py', ''))
+        search_func = getattr(mod, 'search')   
+    
+    eval(query_file, reference_file, output_path, TREC_PATH, search_func)
